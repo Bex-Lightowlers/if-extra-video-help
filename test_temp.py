461b94fc8859704a6d3c3d8265b5fee77702da11
Bex-Lightowlers <bex_holtby@hotmail.co.uk>
2025-04-10 12:39:10 +0100

    Add files via upload

---
+++ test_temp.py
@@ -0,0 +1,13 @@
+import unittest
+from temp import celsius_to_far
+
+class TestTemp(unittest.TestCase):
+    def test_tests_work(self):
+        self.assertEqual(2+2,4)
+
+    def test_celsius_to_far_happy(self):
+        self.assertEqual(celsius_to_far(0), 32)
+        self.assertEqual(celsius_to_far(-40), -40)
+
+if __name__ == "__main__":
+    unittest.main()
